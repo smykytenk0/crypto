fb9b1ec7aaab7e9458304115c4b197c122093420
smykytenk0 <mykytenk00s@gmail.com>
2022-08-05 16:54:05 +0300

    initial commit

--- crypto/src/app/app.component.ts
+++ crypto/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  // TODO: don't forget to create another branch
   // TODO: add material -> Read GET STARTED, create component navigation-component, implement angular material nav inside this component
 }
