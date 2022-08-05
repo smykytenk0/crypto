1e2bfbabc0ce12df54dbfc19587d600393b39153
smykytenk0 <mykytenk00s@gmail.com>
2022-08-05 16:52:40 +0300

    initial commit

---
+++ crypto/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  // TODO: add material -> Read GET STARTED, create component navigation-component, implement angular material nav inside this component
+}
